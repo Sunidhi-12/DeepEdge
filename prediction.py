1a9359615d77e0fb8594fe91eae83af60a3157e3
Sunidhi Gupta <90262774+Sunidhi-12@users.noreply.github.com>
2024-04-19 00:05:30 +0530

    prediction.py

--- prediction.py
+++ prediction.py
@@ -22,18 +22,22 @@ def main():
     test_data_size = 10 
     test_image=[]
     test_label=[]
+    # Generating random test data 
     for _ in range(test_data_size):
         image_t, label_t = dataset_generation()
         test_image.append(image_t)
         test_label.append(label_t)
     test_image = np.array(test_image)
     test_label = np.array(test_label)
+    # Predicting 
     preds=model.predict(test_image)
     x_pred, y_pred = preds[:,0],preds[:,1]
     x_actual, y_actual = test_label[:,0],test_label[:,1]
+    # Scatterplot for the prediction vs actual value
     plt.figure(figsize=(8,6))
     plt.scatter(x_pred, y_pred, color='blue', label='Predicted Coordinates', alpha=0.5)
     plt.scatter(x_actual, y_actual, color='red', label='Actual Coordinates', alpha=0.5)
+    # Scing the scatter plot as jpg
     plt.savefig('scatter_plot.jpg')
 if __name__ == "__main__":
     main()
