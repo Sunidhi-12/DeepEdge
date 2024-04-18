3a750eac92a80ff4c9c8fc8990f63e91ac799d20
Sunidhi Gupta <90262774+Sunidhi-12@users.noreply.github.com>
2024-04-19 00:04:19 +0530

    model_training.py

--- model_training.py
+++ model_training.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 from sklearn.model_selection import train_test_split
 import pickle
-
+# Loadinf the dataset
 def load_dataset(load_path):
     data = np.load(load_path)
     images = data['images']
@@ -19,7 +19,9 @@ def load_dataset(load_path):
 def main():
     dataset_path = "dataset.npz"
     X, y = load_dataset(dataset_path)
+    # Splitting the dataset into train and validation
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size= 0.2, random_state=101)
+    # Model architecture
     model = tf.keras.Sequential([
         Input(shape=(50,50,1)),
         Conv2D(32, (3,3), activation='relu'),
@@ -32,6 +34,7 @@ def main():
     ])
     model.compile(metrics='accuracy',optimizer='adam',loss='mse')
     model.fit(X_train, y_train, validation_data = (X_val,  y_val), epochs=10, batch_size=32)
+    # Model saving
     model.save('model.h5')
 if __name__ == "__main__":
     main()
